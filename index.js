6574cb93412946f1c0eeeb63e52b6178da95ab04
Vlad <vlad_ololo@ukr.net>
2018-01-09 00:32:14 +0200

    set get/hostInfo route

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
+var os = require('os');
 
-app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
   response.sendFile("index.html")
-})
+});
+
+app.get('/hostInfo', function(request, response) {
+	var obj = os.networkInterfaces(); 
+	response.send(obj);
+});
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
-})
+});
+
+
